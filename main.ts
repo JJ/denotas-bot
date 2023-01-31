a8f068342922a2b489437301b72e9bd478db93bb
JJ Merelo <jjmerelo@gmail.com>
2023-01-31 20:13:03 +0100

    Usa la línea de órdenes
    :see_no_evil:

--- main.ts
+++ main.ts
@@ -8,12 +8,12 @@ import { Notas } from "./src/notas.ts";
 const TOKEN = Deno.env.get("DENOTASBOT_TOKEN");
 if (!TOKEN) throw new Error("Bot token is not provided");
 const bot = new TelegramBot(TOKEN);
-const notas = new Notas("data/notas-iv-22-23-ordinaria.csv");
+const notas = new Notas(Deno.args[0] || "data/notas-iv-22-23-ordinaria.csv");
 await notas.setup();
 bot.on(UpdateType.Message, async ({ message }) => {
   console.log(message);
-  const nick = message.from?.username?.toLowerCase();
-  const estasNotas = notas.notas.get(nick as string);
+  const nick: string = message.from?.username?.toLowerCase();
+  const estasNotas = notas.notas.get(nick);
   await bot.sendMessage({
     chat_id: message.chat.id,
     text: `🧑‍🏫 Nota de ${nick}\n❧Proyecto: ${estasNotas?.notaProyecto}\n❧Extra: ${estasNotas?.notaExtra}\n❧Presentación: ${estasNotas?.notaPresentacion}\n❧Final: ${estasNotas?.notaFinal}`,
