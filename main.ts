e1a8d83258ff4eff03799421bc8ceb6b15afc148
JJ Merelo <jjmerelo@gmail.com>
2023-01-31 17:40:00 +0100

    Adapta al funcionamiento real

--- main.ts
+++ main.ts
@@ -12,9 +12,11 @@ const notas = new Notas("data/notas-iv-22-23-ordinaria.csv");
 await notas.setup();
 bot.on(UpdateType.Message, async ({ message }) => {
   console.log(message);
+  const nick = message.from?.username?.toLowerCase();
+  console.log(nick);
   await bot.sendMessage({
     chat_id: message.chat.id,
-    text: notas.notas.get("foo")?.notaFinal,
+    text: notas.notas.get(nick)?.notaFinal,
   });
 });
 
