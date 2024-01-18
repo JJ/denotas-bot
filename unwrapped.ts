bd318fd50ccf78cc1a8c4b963fbf73d55bdaaf94
JJ Merelo <jjmerelo@gmail.com>
2024-01-18 14:10:48 +0100

    Poniendo solo las notas

--- unwrapped.ts
+++ unwrapped.ts
@@ -3,20 +3,9 @@ import {
   UpdateType,
 } from "https://deno.land/x/telegram_bot_api/mod.ts";
 
-import { plot } from "https://deno.land/x/chart/mod.ts";
-
 const TOKEN = Deno.env.get("IV_UNWRAPPED_BOT_TOKEN");
 if (!TOKEN) throw new Error("Bot token is not provided");
 const bot = new TelegramBot(TOKEN);
-
-const reviews = JSON.parse( await Deno.readTextFile( "../reviews.json"));
-const reviewsPerPR = JSON.parse( await Deno.readTextFile( "../reviewers-per-pr.json"));
-
-const reviewedPRs = JSON.parse(await Deno.readTextFile("../reviewed-prs.json"));
-
-const percentiles = JSON.parse( await Deno.readTextFile("../iv-percentiles.json"));
-
-const PRs = JSON.parse( await Deno.readTextFile("../reviewers-per-pr.json"));
 
 const nicks = await Deno.readTextFile("../equivalencia-telegram-github-23-24.csv");
 const lines = nicks.split("\n");
@@ -33,7 +22,13 @@ notasLines.shift();
 const notasMap = new Map<string, number[]>();
 for (const line of notasLines) {
   const [telegram, notaPresentacion,notaExtra,notaProyecto, notaReviews, notaFinal] = line.split("\t");
-  notasMap.set(telegram, [parseNotas(notaPresentacion), parseNotas(notaExtra), parseNotas(notaProyecto), parseFloat(notaReviews), parseFloat(notaFinal)]);
+  console.warn(telegram);
+  notasMap.set(telegram.toLowerCase(),
+  [parseNotas(notaPresentacion),
+    parseNotas(notaExtra),
+    parseNotas(notaProyecto),
+    parseNotas(notaReviews),
+    parseNotas(notaFinal)]);
 }
 console.log(notasMap);
 
@@ -50,51 +45,16 @@ bot.on(UpdateType.Message, async ({ message }) => {
       mensaje =
         `⚠️ Parece que tu nick _${escapeLodash(nick)}_no corresponde a ninguno en GitHub\n`;
     } else {
-      const estosDatos = percentiles[ githubNick ];
-      const reviewsEstudiante = reviews[ githubNick ];
-      const reviewedPREstudiante = Object.keys(reviewedPRs[ githubNick ]).length;
-      const notaPRs = reviewsEstudiante > 14 ? 2 : reviewsEstudiante / 7;
-      if (estosDatos === undefined) {
-        mensaje =
-        `⚠️ Parece que no he encontrado tu nick _${escapeLodash(nick)}_ en la lista\n` +
-        "¿Es posible que te dieras de alta con otro?";
-      } else {
-        const reviewsPerPr = Object.values(PRs[ githubNick ]);
-        console.log(reviewsPerPr);
-        const PRchart = plot( reviewsPerPr );
-        console.log(PRchart);
-        const mediaRevsPRs = reviewsPerPr.reduce( (acc, current ) => { return acc + current } )/reviewsPerPr.length;
-        const [notaPresentacion,notaExtra,notaProyecto, notaReviews, notaFinal] = notasMap.get( githubNick ) || [0,0,0,0,0];
+        const [notaPresentacion,notaExtra,notaProyecto, notaReviews, notaFinal] = notasMap.get( nick ) || [0,0,0,0,0];
         mensaje =
           `🗞 La *_nota provisional_* es *_|| ${escapeDot(notaFinal)} ||_* \n`+
           (notaPresentacion != 0 ?`📊 La nota de la presentación es ${escapeDot(notaPresentacion)}\n`:'') +
           (notaExtra != 0 ?`📊 La nota extra por el hackatón es ${escapeDot(notaExtra)}\n`:"") +
           `📊 La nota del proyecto es ${escapeDot(notaProyecto)}\n` +
-          `📊 La nota por las reviews es ${escapeDot(notaReviews)}\n` +
-          `🎯 *${escapeLodash(nick)}* ha alcanzado el objetivo ${
-            estosDatos["objetivos"]
-          }\n` +
-          `como el *_${escapeDot(
-            estosDatos["percentil"] * 100
-          )}_* % de la clase\n` +
-          `🎓 y por tanto la nota por objetivos es ${escapeDot(
-            estosDatos["nota"]
-          )} sobre 7\n` +
-          `también has hecho *${reviewsEstudiante}* reviews sobre *${reviewedPREstudiante}* PRs lo que corresponde a una nota provisional de _${escapeDot(
-            notaPRs.toFixed(2)
-          )}_\n` +
-          `🗞 y por tanto la nota provisional es _*${escapeDot(
-            (estosDatos["nota"] + notaPRs).toFixed(2)
-          )}*_ sobre 9\n` +
-          "⚠️ Por favor, no te olvides de contestar [la encuesta](https://forms.gle/8oQrHrLk8HnCemsg8)\n" +
-          "*_Pull Requests_*\n" +
-          ` Media de revisiones por PR: ${escapeDot(
-            mediaRevsPRs.toFixed(2)
-          )} \n\n` +
-          `\`\`\`text\n${PRchart.replace(/\./g, "'")}\n\`\`\``;
-      }
+          `📊 La nota por las reviews es ${escapeDot(notaReviews)}\n`;
     }
   }
+
   console.warn(mensaje);
   await bot.sendMessage({
     chat_id: message.chat.id,
@@ -116,9 +76,9 @@ function escapeDot(nota: string | number) {
 }
 
 function parseNotas( nota: string ) {
-  if ( nota === "" ) return 0;
-  console.log( nota );
+  console.warn( "Nota entrada: " + nota );
+  if ( nota === "" || typeof  nota === "undefined") return 0;
   const temp= parseFloat( nota.replace("\"", "").replace(",", "."));
-  console.log( temp );
+  console.log( "Nota salida", temp );
   return( temp)
 }
